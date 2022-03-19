9ebf9476f1b130560346f7954bea781b9771eba0
cpottin <36019063+cpottin@users.noreply.github.com>
2022-03-19 08:12:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,3 @@
-
-
 //  main.cpp
 //  C867 Roster Project
 //
@@ -20,24 +18,26 @@ int main() {
     cout << "C867 - Scripting & Programming: Applications" << endl;
     cout << "Programing Language: C++" << endl;
     cout << "Student ID: 001075237 " << endl;
-    cout << "Name: Cynthia Pottin" << endl;
+    cout << "Name: Cynthia Pottin" << endl<<endl;;
     
     const string studentData[] =
     {
-        "A1, John, Smith, John1989@gm ail.com, 20, 30, 35, 40, SECURITY",
-        "A2, Suzan, Erickson, Erickson_1990@gmailcom, 19, 50, 30, 40, NETWORK",
-        "A3, Jack, Napoli, The_lawyer99yahoo.com, 19, 20, 40, 33, SOFTWARE",
-        "A4, Erin, Black, Erin.black@comcast.net, 22, 50, 58, 40, SECURITY",
-        "A5, Cynthia, Pottin, cpotti2@my.wgu.edu, 46, 30, 45, 60, SOFTWARE"
+        "A1,John, Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+        "A5,Cynthia,Pottin,cpotti2@my.wgu.edu,46,30,45,60,SOFTWARE"
         
     };
     const int numStudents = 5;
     
     Roster roster; //creates roster
     
-    for (int i=0; i < numStudents; i++) roster.parse(studentData[i]);
+    for (int i = 0; i < numStudents; i++) roster.parse(studentData[i]);
+    
     cout << "Displaying all students: " << std::endl;
     roster.printAll();
+    
     cout << std::endl;
     
     for (int i = 0; i < 3; i++)
@@ -50,8 +50,15 @@ int main() {
     roster.printInvalidEmails();
     cout << std::endl;
     
+    for (int i =0; i < numStudents; i++){
     cout << "Displaying average days in course: " << std::endl;
-    roster.printAverageDaysInCourse();
+    
+        roster.printAverageDaysInCourse(roster.classRosterArray[i]->Student::getStudentId());
+    };
+    
+    cout << "Removing studnet with ID A3: " << std::endl;
+    roster.removeStudentByID("A3");
+    cout << std::endl;
     
     cout << "Removing studnet with ID A3: " << std::endl;
     roster.removeStudentByID("A3");
